4cf01631ce0162ec3717dc72efabd58994adf6fa
KennethOnwuachu <kennethonwuachu@gmail.com>
2022-09-07 11:26:29 +0000

    beautiful code

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * This is a simple code that adds two integers
+ * main - entry point
+ * Return: result
+ */
+
+int main(void)
+{
+	int a, b, result;
+
+	a = 1;
+	b = 2;
+	result = a + b;
+
+	return (result);
+}
