76e9353ab90a0b13aa8f6a30b2a14cca21d37a72
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-09-07 06:54:25 -0700

    Task 1

--- main.c
+++ main.c
@@ -1,18 +1,42 @@
-#include <stdio.h>
-
+#include "shell.h"
 /**
- * main - entry point
- * Return: 0
- */
-
+  * main - Entry point
+  * Return: check code
+  */
 int main(void)
 {
-	int a, b, result;
-
-	a = 1;
-	b = 2;
-	result = a + b;
-	printf("%d\n", result);
+	pid_t my_pid;
+	int i, status;
+	ssize_t h = -1, bytes;
+	size_t size = 0;
+	char *cmd, *argv[] = {NULL, NULL, NULL, NULL};
+	/* Display of our shell, getline stores user input in cmd */
+	printf("#cisfun ");
+	bytes = getline(&cmd, &size, stdin);
+	if (bytes == h)
+		exit(0);
+	/* While loop to remove the newline char from cmd */
+	i = 0;
+	while (cmd[i])
+	{
+		if (cmd[i] == '\n')
+			cmd[i] = '\0';
+		i++;
+	}
+	/* Creating a child process for execve */
+	my_pid = fork();
+	argv[0] = cmd;
+	if (my_pid == 0)
+	{
+		if (execve(argv[0], argv, NULL) == -1)
+			perror("Failed");
+	} else
+	{
+		if (bytes == h)
+			exit(0);
+		wait(&status);
+	}
+	main();
 
 	return (0);
 }
---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL
+#define SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+
+#endif
