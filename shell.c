411033362036649a9ac677022899f3a9cf696eb0
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-09-08 01:29:49 -0700

    check

--- shell.c
+++ shell.c
@@ -9,46 +9,52 @@ int runShell()
 	int i, status, j;
 	ssize_t h = -1, bytes;
 	size_t size = 0;
-	char *cmd, *token, *argv[] = {NULL, NULL, NULL, NULL};
-	/* Display of our shell, getline stores user input in cmd */
-	printf("#cisfun ");
-	bytes = getline(&cmd, &size, stdin);
-	if (bytes == h)
-		exit(0);
+	char *cmd = NULL, *token, *argv[] = {NULL, NULL, NULL, NULL};
+	while (1)
+	{
+		/* Display of our shell, getline stores user input in cmd*/
+		i = isatty(STDIN_FILENO);
+		if (i == 1)
+			write(STDOUT_FILENO, "#cisfun ", 8);
+		bytes = getline(&cmd, &size, stdin);
+		if (bytes == h)
+		{
+			free(cmd);
+			return (0);
+		}
 
-	/* While loop to remove the newline char from cmd */
-	i = 0;
-	while (cmd[i])
-	{
-		if (cmd[i] == '\n')
-			cmd[i] = '\0';
-		i++;
-	}
-	if (strcmp(cmd, "env") == 0 || strcmp(cmd, "printenv") == 0)
-	{
-		for (i = 0; environ[i] != NULL; i++)
-			printf("%s\n", environ[i]);
-		runShell();
-		exit(0);
-	}
-	/* Creating a child process for execve */
-	my_pid = fork();
-	token = strtok(cmd, " ");
-	j = 0;
-	while (token != NULL)
-	{
-		argv[j] = token;
-		token = strtok(NULL, " ");
-		j++;
-	}
-	if (my_pid == 0)
-	{
-		if (execve(argv[0], argv, NULL) == -1)
-			perror("./shell");
-	} else
-	{
-		wait(&status);
-		runShell();
+		/* While loop to remove the newline char from cmd */
+		i = 0;
+		while (cmd[i])
+		{
+			if (cmd[i] == '\n')
+				cmd[i] = '\0';
+			i++;
+		}
+		if (strcmp(cmd, "env") == 0 || strcmp(cmd, "printenv") == 0)
+		{
+			for (i = 0; environ[i] != NULL; i++)
+				printf("%s\n", environ[i]);
+			exit(0);
+		}
+		/* Creating a child process for execve */
+		my_pid = fork();
+		token = strtok(cmd, " ");
+		j = 0;
+		while (token != NULL)
+		{
+			argv[j] = token;
+			token = strtok(NULL, " ");
+			j++;
+		}
+		if (my_pid == 0)
+		{
+			if (execve(argv[0], argv, NULL) == -1)
+				perror("./shell");
+		} else
+		{
+			wait(&status);
+		}
 	}
 
 	return (0);
